20c9a57f8ebdd210ee1c1679f0482e8107711d64
kisakyjulian <triumphjana010@gmail.com>
2024-06-03 15:19:08 -0700

    Add files via upload

---
+++ Lion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.company.animal;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lion extends Animal {
+         public Lion(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Roar");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating meat");
+    }
+}
+
+
